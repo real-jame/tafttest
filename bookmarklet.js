76ffa4ebee9e97a0437446cd4d10e7f7123ff6e6
James <james@realja.me>
2023-05-10 09:42:29 -0400

    do stuff

---
+++ bookmarklet.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-labels
+javascript: (function () {
+    document.body.appendChild(document.createElement("script")).src = "https://tafttest.com/taftme.js";
+})();
---
+++ taftme.js
@@ -0,0 +1,40 @@
+(function () {
+    console.log("Taft test loaded");
+    function initMyBookmarklet() {
+        const elements = document.querySelectorAll("*");
+        elements.forEach((element) => {
+            switch (element.className) {
+                case "img": {
+                    element.setAttribute(
+                        "src",
+                        "https://tafttest.com/" + element.width + "x" + element.height + ".png"
+                    );
+                    element.removeAttribute("srcset"); // TODO: replace individual images in srcset with Taft
+                    break;
+                }
+                case "picture": {
+                    const sources = element.querySelectorAll("source");
+                    sources.forEach(source => {
+                        const sourceSrc = source.srcset;
+                        const img = new Image();
+
+                        img.onload = function () {
+                            source.setAttribute(
+                                "srcset",
+                                "https://tafttest.com/" + img.width + "x" + img.height + ".png"
+                            );
+                        }
+
+                        img.src = sourceSrc;
+                    })
+                    break;
+                }
+                default: {
+                    // TODO: replace CSS background image elements
+                    break;
+                }
+            }
+        });
+    }
+    initMyBookmarklet();
+});
