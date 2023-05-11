6843d4541aac73997a126881cb788999f8f4c4fd
James <james@realja.me>
2023-05-11 15:49:46 -0400

    SVG support and improve functionality

--- taftme.js
+++ taftme.js
@@ -1,40 +1,57 @@
-(function () {
-    console.log("Taft test loaded");
-    function initMyBookmarklet() {
-        const elements = document.querySelectorAll("*");
-        elements.forEach((element) => {
-            switch (element.className) {
-                case "img": {
-                    element.setAttribute(
-                        "src",
-                        "https://tafttest.com/" + element.width + "x" + element.height + ".png"
-                    );
-                    element.removeAttribute("srcset"); // TODO: replace individual images in srcset with Taft
+// (function () {
+console.log("Taft test loaded");
+function initMyBookmarklet() {
+    const elements = document.querySelectorAll("*");
+    elements.forEach((element) => {
+        switch (element.nodeName.toLowerCase()) {
+            case "img": {
+                console.log("Tafting", element);
+                element.src = "https://tafttest.com/" + element.width + "x" + element.height + ".png";
+                element.removeAttribute("srcset"); // TODO: replace individual images in srcset with Taft
+                break;
+            }
+            case "picture": {
+                console.log("Tafting", element);
+                const sources = element.querySelectorAll("source");
+                sources.forEach(source => {
+                    const sourceSrc = source.srcset;
+                    const img = new Image();
+
+                    img.onload = function () {
+                        source.srcset = "https://tafttest.com/" + img.width + "x" + img.height + ".png";
+                    }
+
+                    img.src = sourceSrc;
+                })
+                break;
+            }
+            case "svg": {
+                console.log("Tafting", element);
+                const svgDimensions = element.getBBox();
+                if (svgDimensions.width < .5 || svgDimensions.height < .5) {
                     break;
                 }
-                case "picture": {
-                    const sources = element.querySelectorAll("source");
-                    sources.forEach(source => {
-                        const sourceSrc = source.srcset;
-                        const img = new Image();
-
-                        img.onload = function () {
-                            source.setAttribute(
-                                "srcset",
-                                "https://tafttest.com/" + img.width + "x" + img.height + ".png"
-                            );
-                        }
-
-                        img.src = sourceSrc;
-                    })
-                    break;
-                }
+                const img = document.createElement("img");
+                img.src = "https://tafttest.com/" + Math.round(svgDimensions.width) + "x" + Math.round(svgDimensions.height) + ".png";
+                element.replaceWith(img);
+                break;
             }
-            const style = window.getComputedStyle(element);
-            if (style.backgroundImage !== 'none') {
-                element.style.backgroundImage = 'url(newImageURL)';
+        }
+        const style = window.getComputedStyle(element);
+        if (style.backgroundImage !== 'none') {
+            console.log("Tafting", element);
+            const img = new Image();
+
+            img.onload = function () {
+                console.log("Loaded");
+                element.style.backgroundImage =
+                    "https://tafttest.com/" + img.width + "x" + img.height + ".png";
             }
-        });
-    }
-    initMyBookmarklet();
-});
+
+            img.src = element.style.backgroundImage.replace(/^url\(['"]?(.+?)['"]?\)/, '$1');
+        }
+    });
+}
+initMyBookmarklet();
+console.log("Done!");
+// });
